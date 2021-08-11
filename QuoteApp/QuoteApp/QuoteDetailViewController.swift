74c2c25cf2d8146e224d66de6700632cdd71f146
Enrico Rossini <erossini@users.noreply.github.com>
2021-08-11 19:35:17 +0100

    First commit with bug

---
+++ QuoteApp/QuoteApp/QuoteDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QuoteDetailViewController.swift
+//  QuoteApp
+//
+//  Created by Enrico Rossini on 11/08/2021.
+//
+
+import UIKit
+
+class QuoteDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        if let quoteViewController = segue.destination as? QuoteDetailViewController {
+            if let selectedQuote = sender as? String {
+                quoteViewController.title = selectedQuote
+            }
+        }
+    }
+}
